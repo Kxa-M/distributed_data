ac3d26de105ea867db94e4033ade6c51b4f4704b
Kxa-M <eleve8icnscarron@gmail.com>
2024-11-14 21:09:56 +0000

    add false to no partition

--- pyspark/bash-of-bashes.sh
+++ pyspark/bash-of-bashes.sh
@@ -1,13 +1,13 @@
-#$1 links file  | $2 name cluster | $3 number workers | $4 number of iterations
+#$1 links file  | $2 name cluster | $3 number workers | $4 number of iterations | $5 boolean to enable partitionner
 
 ## notype
 
 nohup bash run.sh small_page_links.nt cluster-notype-partition 2 3 true > $PATH_BUCKET/output_sm_notype_partition_2w.log 2>&1 &
 
-nohup bash run.sh small_page_links.nt cluster-notype-no-partition 0 3 true > $PATH_BUCKET/output_sm_notype_no_partition_0w.log 2>&1 &
+nohup bash run.sh small_page_links.nt cluster-notype-no-partition 0 3 false > $PATH_BUCKET/output_sm_notype_no_partition_0w.log 2>&1 &
 
 ## DF
 
 nohup bash run-df.sh small_page_links.nt cluster-df-partition 0 3 true > $PATH_BUCKET/output_sm_df_partition_0w.log 2>&1 &
 
-nohup bash run-df.sh small_page_links.nt cluster-df-no-partition 0 3 true > $PATH_BUCKET/output_sm_df_no_partition_0w.log 2>&1 &
+nohup bash run-df.sh small_page_links.nt cluster-df-no-partition 0 3 false > $PATH_BUCKET/output_sm_df_no_partition_0w.log 2>&1 &
