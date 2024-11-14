0fc510d83d24e7227964275e5ef7c35b8e0348cc
Kxa-M <eleve8icnscarron@gmail.com>
2024-11-14 14:22:33 +0000

    change the convention name of file

--- pyspark/bash-of-bashes.sh
+++ pyspark/bash-of-bashes.sh
@@ -1,3 +1,13 @@
-nohup bash run.sh small_page_links.nt test-cluster 2 3 > output_sm_23.log 2>&1 &
+#$1 links file  | $2 name cluster | $3 number workers | $4 number of iterations
 
-nohup bash run.sh small_page_links.nt cluster-test 0 3 > output_sm_03.log 2>&1 &
+## notype
+
+nohup bash run.sh small_page_links.nt cluster-notype-partition 2 3 > $PATH_BUCKET/output_sm_notype_partition_2w.log 2>&1 &
+
+nohup bash run.sh small_page_links.nt cluster-notype-no-partition 0 3 > $PATH_BUCKET/output_sm_notype_no_partition_0w.log 2>&1 &
+
+## DF
+
+nohup bash run-df.sh small_page_links.nt cluster-df-partition 0 3 > $PATH_BUCKET/output_sm_df_partition_0w.log 2>&1 &
+
+nohup bash run-df.sh small_page_links.nt cluster-df-no-partition 0 3 > $PATH_BUCKET/output_sm_df_no_partition_0w.log 2>&1 &
