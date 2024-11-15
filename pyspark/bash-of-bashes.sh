45a968b5304be96229c2a31726ecb02a9d9139a3
Théo <komete17@gmail.com>
2024-11-15 14:07:04 +0000

    Bash to run them all

--- pyspark/bash-of-bashes.sh
+++ pyspark/bash-of-bashes.sh
@@ -2,12 +2,21 @@
 
 ## notype
 
-nohup bash run.sh small_page_links.nt cluster-notype-partition 2 3 true > $PATH_BUCKET/output_sm_notype_partition_2w.log 2>&1 &
+nohup bash run.sh page_links_en.nt.bz2 cluster-notype-partition-0w 0 3 true > output_pl_notype_partition_0w.log 2>&1 &
+nohup bash run.sh page_links_en.nt.bz2 cluster-notype-partition-2w 2 3 true > output_pl_notype_partition_2w.log 2>&1 &
+nohup bash run.sh page_links_en.nt.bz2 cluster-notype-partition-4w 4 3 true > output_pl_notype_partition_4w.log 2>&1 &
 
-nohup bash run.sh small_page_links.nt cluster-notype-no-partition 0 3 false > $PATH_BUCKET/output_sm_notype_no_partition_0w.log 2>&1 &
+nohup bash run.sh page_links_en.nt.bz2 cluster-notype-no-partition-0w 0 3 false > output_pl_notype_no_partition_0w.log 2>&1 &
+nohup bash run.sh page_links_en.nt.bz2 cluster-notype-no-partition-2w 2 3 false > output_pl_notype_no_partition_2w.log 2>&1 &
+nohup bash run.sh page_links_en.nt.bz2 cluster-notype-no-partition-4w 4 3 false > output_pl_notype_no_partition_4w.log 2>&1 &
+
 
 ## DF
 
-nohup bash run-df.sh small_page_links.nt cluster-df-partition 0 3 true > $PATH_BUCKET/output_sm_df_partition_0w.log 2>&1 &
+nohup bash run-df.sh page_links_en.nt.bz2 cluster-df-partition-0w 0 3 true > output_pl_df_partition_0w.log 2>&1 &
+nohup bash run-df.sh page_links_en.nt.bz2 cluster-df-partition-2w 2 3 true > output_pl_df_partition_2w.log 2>&1 &
+nohup bash run-df.sh page_links_en.nt.bz2 cluster-df-partition-4w 4 3 true > output_pl_df_partition_4w.log 2>&1 &
 
-nohup bash run-df.sh small_page_links.nt cluster-df-no-partition 0 3 false > $PATH_BUCKET/output_sm_df_no_partition_0w.log 2>&1 &
+nohup bash run-df.sh page_links_en.nt.bz2 cluster-df-no-partition-0w 0 3 false > output_pl_df_no_partition_0w.log 2>&1 &
+nohup bash run-df.sh page_links_en.nt.bz2 cluster-df-no-partition-2w 2 3 false > output_pl_df_no_partition_2w.log 2>&1 &
+nohup bash run-df.sh page_links_en.nt.bz2 cluster-df-no-partition-4w 4 3 false > output_pl_df_no_partition_4w.log 2>&1 &
